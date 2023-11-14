f5aa3931a3bd193a39e7f0297fa3ecaa479f70d1
Bizykova <bizykova.daria@gmail.com>
2023-11-14 20:12:09 +0300

    modified lesson eight

--- src/test/java/lesson_eight/FormPracticeTest.java
+++ src/test/java/lesson_eight/FormPracticeTest.java
@@ -33,7 +33,7 @@ public class FormPracticeTest extends BaseTest {
                 .setCurrentAddress(fakerBox.streetAddress)
                 .execute()
                 .clickState(fakerBox.state)
-                .clickCity(fakerBox.getCity(fakerBox.state))
+                .clickCity(fakerBox.city)
                 .clickSubmit();
         //assert
         checkResult.checkResult("Student Name", fakerBox.firstName+" "+fakerBox.lastName)
@@ -45,7 +45,7 @@ public class FormPracticeTest extends BaseTest {
                 .checkResult("Hobbies",fakerBox.hobbies)
                 .checkResult("Picture", fileName)
                 .checkResult("Address", fakerBox.streetAddress)
-                .checkResult("State and City",fakerBox.state+ " "+fakerBox.getCity(fakerBox.state));
+                .checkResult("State and City",fakerBox.state+ " "+fakerBox.city);
     }
 
     @Test
--- src/test/java/lesson_eight/faker/FakerBox.java
+++ src/test/java/lesson_eight/faker/FakerBox.java
@@ -9,28 +9,28 @@ public class FakerBox {
     public String userEmai = faker.internet().emailAddress();
     public String streetAddress = faker.address().fullAddress();
     public String gender = faker.options().option("Male", "Female", "Other");
-    //public String phone = Long.toString(faker.number().numberBetween(1000000000L,7999999999L));
     public String phone = faker.phoneNumber().subscriberNumber(10);
     public String year = Integer.toString(faker.number().numberBetween(2000, 2024));
     public String month = faker.options().option("February", "March", "April", "May", "June", "July", "August", "September");
     public String subjects = faker.options().option("Computer Science", "Match", "History", "English");
     public String hobbies = faker.options().option("Sports", "Reading", "Music");
-    public String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana");
-    public String cityNCR = faker.options().option("Delhi", "Noida", "Gurgaon");
-    public String cityUttar = faker.options().option("Agra", "Merrut", "Lucknow");
-    public String cityHaryana = faker.options().option("Panipart", "Karnal");
+    public String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana","Rajasthan");
+    public String city = getCity(state);
 
-    public String getCity(String value) {
-        switch (value) {
+    public String getCity(String state) {
+        switch (state) {
             case "NCR":
-                return cityNCR;
+                return faker.options().option("Delhi", "Gurgaon", "Noida");
             case "Uttar Pradesh":
-                return cityUttar;
+                return faker.options().option("Agra", "Lucknow", "Merrut");
             case "Haryana":
-                return cityHaryana;
+                return faker.options().option("Karnal", "Panipat");
+            case "Rajasthan":
+                return faker.options().option("Jaipur", "Jaiselmer");
+            default:
+                return null;
         }
-
-        return "";
     }
-
 }
+
+
